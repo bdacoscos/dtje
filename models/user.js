7d4b90ff0ef61e20c71a5148e64a064a802e029d
Griffin Scott <griffinoscott@gmail.com>
2017-11-17 15:31:52 -0800

    fixed models

--- models/user.js
+++ models/user.js
@@ -4,8 +4,10 @@ var Schema = mongoose.Schema;
 var restaurants = require('./restaurant');
 
 var userSchema = new Schema({
-  address: String,
+  location: String,
+  googleId: String,
+  avatar: String,
   favorites: [{ type: Schema.Types.ObjectId, ref: 'Restaurant' }]
-})
+});
 
 module.exports = mongoose.model('User', userSchema);
