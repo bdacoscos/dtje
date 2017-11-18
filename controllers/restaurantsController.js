7ad7f65e603749c9e014fdcfaa11a45905ce151e
Dean Hamann <hamann.dean@gmail.com>
2017-11-18 12:50:46 -0800

    Yelp API functionality

--- controllers/restaurantsController.js
+++ controllers/restaurantsController.js
@@ -1,21 +1,32 @@
 var Restaurant = ('./models/restaurant');
 
+// function index (req,res){
+//     // Restaurant.findById({yelpId}, function(err, restaurant){
+//     //     if (err) return res.status(err.statusCode || 500).json(err);
+//     //     res.json(restaurant);
+//     // });
+// }
+
+
 function index (req,res){
-    // Restaurant.findById({yelpId}, function(err, restaurant){
-    //     if (err) return res.status(err.statusCode || 500).json(err);
-    //     res.json(restaurant);
-    // });
-}
+    var options = {
+        url: "https://api.yelp.com/v3/businesses/search?term=restaurant&location=boulder",
+        headers: {
+          'Authorization': 'Bearer ' + process.env.access_token
+        }
+      };
+        
+};
 
  
 
 
-var options = {
-    url: rootURL + 'search/users?q=' + req.body.search + ' in:fullname',
-    headers: {
-      'Authorization': 'Bearer ' + process.env.access_token
-    }
-  };
+// var options = {
+//     url: rootURL + 'search/users?q=' + req.body.search + ' in:fullname',
+//     headers: {
+//       'Authorization': 'Bearer ' + process.env.access_token
+//     }
+//   };
 
 
 
@@ -23,5 +34,4 @@ var options = {
 
 module.exports = {
     index, 
-
 }
