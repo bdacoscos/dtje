f80efb27d3e01d2b40b6b558051c55da42cb5ff5
Griffin Scott <griffinoscott@gmail.com>
2017-11-20 16:01:18 -0800

    added users show page to /users/show

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,9 +1,9 @@
 var User = require('../models/user');
 var Restaurant = require('../models/restaurant')
 
-function show (req, res) {
+function show (req, res, next) {
   console.log(req.user);
-  res.render('/show', { user: req.user });
+  res.render('show', { user: req.user });
 }
 
 
