d0fcc2c1f900a36dbe3c2e873220bd735709092d
bdacoscos <briana.dacoscos@gmail.com>
2017-11-20 16:09:38 -0800

    Revert "added users show page to /users/show"

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,9 +1,9 @@
 var User = require('../models/user');
 var Restaurant = require('../models/restaurant')
 
-function show (req, res, next) {
+function show (req, res) {
   console.log(req.user);
-  res.render('show', { user: req.user });
+  res.render('/show', { user: req.user });
 }
 
 
